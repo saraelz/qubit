11fd0d0d8b9b4a31d9913ee5c3d626c7e5c557e2
saraelz <sara.elzeiny@gmail.com>
2024-04-17 07:51:09 -0700

    Split up test cases for readability function.

--- app.py
+++ app.py
@@ -3,141 +3,124 @@ import os.path
 from dataclasses import dataclass
 import gdspy
 from SimpleQubit import SimpleQubit
-
-
-def _validate_file(filename: str) -> bool:
-    """
-    Returns True if file exists in directory.
-    
-    Args:
-        filename (str): This is the name of the input file.
-    Returns:
-        bool: Returns True if file is found.
-    """
-    return os.path.isfile(filename)
+import unittest
 
 
 @dataclass
 class Circle:
-    """
-    This is a test class for circle, especially to organize and instantiate variables
-    """
     radius: float
 
 
 @dataclass
 class Rectangle:
-    """
-    This is a test class for rectangles, especially to organize and instantiate variables
-    """
     width: float
     height: float
 
+    def equals(self, other):
+        return (math.isclose(other.width, self.width) and math.isclose(other.height, self.height)) or (math.isclose(other.height,self.width) and math.isclose(other.width,self.height))
 
-def main():
-    """Initialize Dummy Data"""
-    junction = Rectangle(2., .4)
-    wire = Rectangle(.3, 10.)
-    connection = Circle(4.)
-    offset = junction.width/10
-    layers = {
-        "connection_layer": 0,
-        "wire_layer": 1,
-        "junction_layer": 2
-    }
 
-    q = SimpleQubit(connection_radius=connection.radius, wire_height=wire.height, wire_width=wire.width,
-                    junction_offset=offset, junction_height=junction.height, junction_width=junction.width, **layers)
-    layout = q.draw()
+def _validate_file(filename: str) -> bool:
+    return os.path.isfile(filename)
 
-    """Test cases"""
-    shapes = q.get_polygonsets()
 
-    print("Test 1: assert that the circuit has at least 5 polygons for a simple qubit")
-    assert len(shapes) >= 5
+def _calculate_gdspy_rectangle_dimensions(rectangle: gdspy.polygon.Rectangle) -> Rectangle:
+    corners = rectangle.polygons[0]
+    dist1 = math.dist(corners[0], corners[1])
+    dist2 = math.dist(corners[1], corners[2])
+    return Rectangle(dist1, dist2)
 
-    print("Test 2: assert that there are at least 3 layers in the qubit layout")
-    assert len(layout.get_layers()) >= 3
 
-    print("Test 2b: assert that the actual layers match the expected layers")
-    assert (set(layout.get_layers()) == set(layers.values()))
+class TestSimpleQubit(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        self.junction = Rectangle(2.0, 0.4)
+        self.wire = Rectangle(0.3, 10.0)
+        self.connection = Circle(4.0)
+        self.offset = self.junction.width / 10.0
+        self.layers = {"connection_layer": 0, "wire_layer": 1, "junction_layer": 2}
 
-    for shape in shapes:
+        self.qubit = SimpleQubit(
+            connection_radius=self.connection.radius,
+            wire_height=self.wire.height,
+            wire_width=self.wire.width,
+            junction_offset=self.offset,
+            junction_height=self.junction.height,
+            junction_width=self.junction.width,
+            **self.layers
+        )
+        self.layout = self.qubit.draw()
+        self.shapes = self.qubit.get_polygonsets()
 
-        print("Test 3: assert that area > 0")
-        assert shape.area() > 0
+    def test_polygon_count(self):
+        self.assertGreaterEqual(len(self.shapes), 5)
 
-        print("Test 3-10: Check the layers")
-        assert shape.layers
+    def test_layer_count(self):
+        self.assertGreaterEqual(len(self.layout.get_layers()), 3)
 
-        for layer in shape.layers:
-            if layer == layers['connection_layer']:
-                print("Found Connection layer: Executing connection tests")
-                print("Test 4: assert that connection is a round shape type")
-                assert isinstance(shape, gdspy.polygon.Round)
+    def test_drawing_has_only_expected_layers(self):
+        self.assertEqual(set(self.layout.get_layers()), set(self.layers.values()))
 
-                print(
-                    "Test 5: calculate radius through area() and ensure that it matches expected_radius")
-                actual_radius = round(math.sqrt(shape.area()/math.pi), 1)
-                assert 4. == actual_radius
+    def test_shape_area_greater_than_0(self):
+        for shape in self.shapes:
+            self.assertGreater(shape.area(), 0)
 
-            elif layer == layers['junction_layer']:
-                print("Found Junction layer. Executing junction tests...")
-                print("Test 6: assert that junction is a rectangular shape type")
-                assert isinstance(shape, gdspy.polygon.Rectangle)
+    def test_shape_type(self):
+        for shape in self.shapes:
+            self.assertTrue(shape.layers)
+            for layer in shape.layers:
+                if layer == self.layers["connection_layer"]:
+                    self.assertIsInstance(shape, gdspy.polygon.Round)
+                elif layer == self.layers["junction_layer"]:
+                    self.assertIsInstance(shape, gdspy.polygon.Rectangle)
+                elif layer == self.layers["wire_layer"]:
+                    self.assertIsInstance(shape, gdspy.polygon.Rectangle)
 
-                print(
-                    "Test 7: get corners of rectangle to compare actual and expected distances")
-                corners = shape.polygons[0]
-                length = round(math.dist(corners[0], corners[1]), 1)
-                width = round(math.dist(corners[1], corners[2]), 1)
-                assert (length == junction.width and width == junction.height) or (
-                    length == junction.height and width == junction.width)
+    def test_shape_dimensions(self):
+        for shape in self.shapes:
+            self.assertTrue(shape.layers)
+            for layer in shape.layers:
+                if layer == self.layers["connection_layer"]:
+                    actual_radius = math.sqrt(shape.area() / math.pi)
+                    self.assertAlmostEqual(self.connection.radius, actual_radius, places=1)
+                elif layer == self.layers["junction_layer"]:
+                    actual = _calculate_gdspy_rectangle_dimensions(shape)
+                    self.assertTrue(Rectangle(self.junction.width,self.junction.height).equals(actual))
+                elif layer == self.layers["wire_layer"]:
+                    actual = _calculate_gdspy_rectangle_dimensions(shape)
+                    self.assertTrue(Rectangle(self.wire.width,self.wire.height).equals(actual))
 
-            elif layer == layers['wire_layer']:
-                print("Found Wire layer. Executing Wire tests...")
-                print("Test 8: check wire is a rectangular shape type")
-                assert isinstance(shape, gdspy.polygon.Rectangle)
+    def test_offset_range(self):
+        self.assertGreater(self.offset, 0)
+        self.assertLessEqual(self.offset, self.junction.width / 2)
 
-                print(
-                    "Test 9: get corners of rectangle to compare actual and expected distances")
-                corners = shape.polygons[0]
-                length = round(math.dist(corners[0], corners[1]), 1)
-                width = round(math.dist(corners[1], corners[2]), 1)
-                assert (length == wire.width and width == wire.height) or (
-                    length == wire.height and width == wire.width)
+    def test_polygon_no_gaps(self):
+        joined_polygon_set = gdspy.boolean(self.shapes, None, "or", max_points=0)
+        test_cell = gdspy.GdsLibrary().new_cell("TEST_POLYGON_NO_GAPS").add(joined_polygon_set)
+        test_cell.write_svg('TEST_POLYGON_NO_GAPS.svg')
+        self.assertEqual(len(joined_polygon_set.polygons), 1)
+        
+    def test_svg_export(self):
+        output_svg = "output.svg"
+        self.qubit.to_svg(filename=output_svg)
+        self.assertTrue(_validate_file(filename=output_svg))
 
-    print("End loop")
-    print("Test 11: Check offset is within the appropriate range")
-    assert offset > 0 and offset <= junction.width/2
+    def test_gds_export(self):
+        output_gds = "output.gds"
+        self.qubit.to_gds(filename=output_gds)
+        self.assertTrue(_validate_file(filename=output_gds))
 
-    print("Test 12: Check all polygons connected without gaps between polygons")
-    combined_polygon_set = gdspy.boolean(shapes, None, 'or')
-    assert len(combined_polygon_set.polygons) == 1
+    def test_serialization_and_deserialization(self):
+        output_json = "output.json"
+        self.assertTrue(self.qubit.serialize(filename=output_json))
+        self.assertTrue(_validate_file(filename=output_json))
+        self.assertTrue(self.qubit.from_json_file(filename=output_json))
 
-    """Export to svg file and gds file"""
-    output_svg = 'output.svg'
-    output_gds = 'output.gds'
-    output_json = 'output.json'
-
-    print("Test 13: Export svg and check that file exists.")
-    q.to_svg(filename=output_svg)
-    assert _validate_file(filename=output_svg)
-
-    print("Test 14: Export gds and check that file exists.")
-    q.to_gds(filename=output_gds)
-    assert _validate_file(filename=output_gds)
-
-    """Json tests"""
-    print("Test 15: serialize object, export to json")
-    q.serialize(filename=output_json)
-    print("Test 15b: check that json file exists")
-    assert _validate_file(filename=output_json)
-    print('Test 16: validate that json matches json schema and deserialize object from json file')
-    q.from_json_file(filename=output_json)
-
-    print('All tests passed.')
+    def call_draw_function_repeatedly(self):
+        self.qubit.draw()
+        self.qubit.draw()
+        return True
 
 
 if __name__ == "__main__":
-    main()
+    unittest.main()
